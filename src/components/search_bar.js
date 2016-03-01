1a61b7132d27c2cfeae0f14824f64660a7c34f87
Randy Torres <torresr14@gmail.com>
2016-02-29 21:03:25 -0800

    Refactored SearchBar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
-		constructor(props) {
-				super(props);
+	constructor(props) {
+		super(props);
 
-				this.state = { term: '' };
-		}
+		this.state = { term: '' };
+	}
 
 		render() {
-				return (
-						<div className="search-bar">
-								<input
-									value={this.state.term}
-									onChange={event => this.onInputChange(event.target.value)} />
-						</div>
-				 );
+			return (
+				<div className="search-bar">
+					<input
+						value={this.state.term}
+						onChange={event => this.onInputChange(event.target.value)} />
+				</div>
+			);
 		}
 
 	onInputChange(term) {
-     this.setState({term});
-		 this.props.onSearchTermChange(term);
+    this.setState({term});
+		this.props.onSearchTermChange(term);
 	}
 }
 
